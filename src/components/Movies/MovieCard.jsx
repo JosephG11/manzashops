a352e14e97cea7743385d4ff77c4f012957f7454
bsegers <briansegs@gmail.com>
2025-01-31 14:02:20 -0500

    feat: Add data to MovieCard

--- src/components/Movies/MovieCard.jsx
+++ src/components/Movies/MovieCard.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
+import { RxCross1 } from "react-icons/rx";
+
 const movieListing = {
   id: "ml00001",
   image: {
     src: "/manzashops/assets/ad/movie24.jpeg",
-    alt: "",
+    alt: "Witch Poster",
   },
   listingDate: {
     month: "Dec",
@@ -13,21 +16,108 @@ const movieListing = {
   movieDate: "October 4, 2024",
   movieDescription:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel turpis at nulla tincidunt sodales. Integer tincidunt, sapien id volutpat tincidunt, libero sapien suscipit nisl, eget fermentum justo lorem in urna. Aliquam erat volutpat. Vestibulum ut libero ac sapien venenatis sagittis non nec quam. Curabitur euismod, nulla at pharetra auctor, nisi turpis vehicula justo, ac scelerisque mauris erat id turpis.",
+  ticketsLink: "/",
+  buttons: [
+    {
+      title: "trailers",
+      links: [
+        {
+          title: "fandango",
+          href: "https://Fandango.com",
+        },
+        {
+          title: "Atom tickets",
+          href: "https://atomtickets.com",
+        },
+        {
+          title: "alpine Cinema",
+          href: "https://alpinecinema.com",
+        },
+        {
+          title: "AMC",
+          href: "https://amctheatres.com",
+        },
+        {
+          title: "Regal",
+          href: "https://regmovies.com",
+        },
+      ],
+    },
+    {
+      title: "tickets",
+      links: [
+        {
+          title: "Atom tickets",
+          href: "https://atomtickets.com",
+        },
+        {
+          title: "alpine Cinema",
+          href: "https://alpinecinema.com",
+        },
+        {
+          title: "fandango",
+          href: "https://Fandango.com",
+        },
+
+        {
+          title: "AMC",
+          href: "https://amctheatres.com",
+        },
+        {
+          title: "Regal",
+          href: "https://regmovies.com",
+        },
+      ],
+    },
+    {
+      title: "resources",
+      links: [
+        {
+          title: "alpine Cinema",
+          href: "https://alpinecinema.com",
+        },
+        {
+          title: "AMC",
+          href: "https://amctheatres.com",
+        },
+        {
+          title: "Regal",
+          href: "https://regmovies.com",
+        },
+        {
+          title: "fandango",
+          href: "https://Fandango.com",
+        },
+        {
+          title: "Atom tickets",
+          href: "https://atomtickets.com",
+        },
+      ],
+    },
+  ],
 };
 
 const MovieCard = () => {
+  const [activeId, setActiveId] = useState(null);
+
+  const toggleById = (id) => {
+    setActiveId((prevId) => (prevId === id ? null : id));
+  };
+
   const {
     image: { src, alt },
     listingDate: { month, day, year },
     movieTitle,
     movieDate,
     movieDescription,
+    ticketsLink,
+    buttons,
   } = movieListing;
 
   return (
     <div className="flex">
       <div className="w-16 leading-5">
-        <p className="text-3xl font-bold text-red-400">{month}</p>
+        <p className="text-3xl font-bold text-red-400 capitalize">{month}</p>
         <p className="text-center text-2xl text-white">{day}</p>
         <p className="text-center text-sm text-white">{year}</p>
       </div>
@@ -40,20 +130,20 @@ const MovieCard = () => {
             <div className="flex w-full">
               <div className="w-full text-white">
                 <p>
-                  Date: <span>{movieDate}</span>
+                  Date: <span className="capitalize">{movieDate}</span>
                 </p>
                 <p>
-                  Title: <span>{movieTitle}</span>
+                  Title: <span className="capitalize">{movieTitle}</span>
                 </p>
               </div>
+
               <div className="flex w-full justify-center items-center">
-                <button
-                  type="button"
-                  onClick={() => {}}
+                <a
+                  href={ticketsLink}
                   className="bg-red-500 text-white rounded-[10px] px-4 py-2 w-48 h-10 text-center"
                 >
-                  test
-                </button>
+                  Tickets
+                </a>
               </div>
             </div>
 
@@ -62,30 +152,44 @@ const MovieCard = () => {
             </div>
           </div>
 
-          <div className="bg-black w-full p-2 flex justify-around rounded-br-[10px]">
-            <button
-              type="button"
-              onClick={() => {}}
-              className="bg-white text-black rounded-[10px] px-4 py-2 w-32 text-center"
-            >
-              test
-            </button>
+          <div className="bg-black w-full h-14 p-2 flex justify-around rounded-br-[10px]">
+            {buttons.map(({ title, links }) => (
+              <div key={title}>
+                <button
+                  type="button"
+                  onClick={() => toggleById(title)}
+                  className="bg-white text-black rounded-[10px] px-4 py-2 w-32 h-10 text-center capitalize"
+                >
+                  {title}
+                </button>
 
-            <button
-              type="button"
-              onClick={() => {}}
-              className="bg-white text-black rounded-[10px] px-4 py-2 w-32 text-center"
-            >
-              test
-            </button>
-
-            <button
-              type="button"
-              onClick={() => {}}
-              className="bg-white text-black rounded-[10px] px-4 py-2 w-32 text-center"
-            >
-              test
-            </button>
+                {activeId === title && (
+                  <div
+                    id={title}
+                    className="flex flex-col bg-white items-center rounded-[10px] relative top-1 pb-2 pt-5 px-3"
+                  >
+                    <button
+                      type="button"
+                      onClick={() => toggleById(title)}
+                      className="absolute right-2 top-2"
+                    >
+                      <RxCross1 />
+                    </button>
+                    {links.map(({ title, href }) => (
+                      <a
+                        key={title}
+                        href={href}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="capitalize"
+                      >
+                        {title}
+                      </a>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))}
           </div>
         </div>
       </div>
